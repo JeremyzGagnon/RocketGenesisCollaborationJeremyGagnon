c674fcef0455e45f4861d5a94de10d757c58d6d8
Jeremy Gagnon <jeremyzgagnon@gmail.com>
2023-02-10 15:27:54 -0400

    Tableau trier et filtrer

--- residential.js
+++ residential.js
@@ -1,14 +1,10 @@
 let first_name = document.querySelectorAll(".first-name");
 let last_name = document.querySelectorAll(".last-name");
-let rating = document.querySelectorAll(".rating");
+let rating = document.querySelectorAll(".ratings");
 let fee = document.querySelectorAll(".fee");
+let region = document.querySelectorAll(".region");
 
 let top_tier = [];
-
-
-
-
-
 
 // GET method
 fetch("http://99.79.77.144:3000/api/agents", {
@@ -21,6 +17,7 @@ fetch("http://99.79.77.144:3000/api/agents", {
     return Promise.reject(res)
 })
 .then(data => {
+    
     for(let i = 0; i < data.length; i++) {
         // console.log(data[i])
         if (data[i].rating >= 95) {
@@ -35,6 +32,7 @@ fetch("http://99.79.77.144:3000/api/agents", {
         last_name[i].innerHTML = top_tier[i].last_name;
         rating[i].innerHTML = top_tier[i].rating;
         fee[i].innerHTML = top_tier[i].fee;
+        region[i].innerHTML = top_tier[i].region;
 
     }
 
@@ -42,4 +40,80 @@ fetch("http://99.79.77.144:3000/api/agents", {
 })
 .catch(res => console.error(res.status))
 
+// Sorts the table
 
+function sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("table");
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // Start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /* Loop through all table rows (except the
+      first, which contains table headers): */
+      for (i = 1; i < (rows.length - 1); i++) {
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+        /* Get the two elements you want to compare,
+        one from current row and one from the next: */
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        /* Check if the two rows should switch place,
+        based on the direction, asc or desc: */
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        // Each time a switch is done, increase this count by 1:
+        switchcount ++;
+      } else {
+        /* If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again. */
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
+
+function filterTable() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[4];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+  
